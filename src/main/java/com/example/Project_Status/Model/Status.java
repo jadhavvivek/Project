3f3433f010d77a3a59aa01838ae40497f45b7fa8
vivek <vivek@Vivek>
2023-04-20 11:23:46 +0530

    new

---
+++ src/main/java/com/example/Project_Status/Model/Status.java
@@ -0,0 +1,35 @@
+package com.example.Project_Status.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Status {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+
+	@Column(name="Status")
+	private String status;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	
+}
